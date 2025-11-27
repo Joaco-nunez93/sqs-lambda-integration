189c5100908d914aa6b03f5dfa7a2f90fb1a779d
Joaquin Nahuel <joaco.aws.senior@gmail.com>
2025-11-27 11:49:00 -0300

    Delete sqs-lambda-integration directory

--- sqs-lambda-integration/bin/sqs-integration.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib/core';
-import { SqsIntegrationStack } from '../lib/sqs-integration-stack';
-
-const app = new cdk.App();
-new SqsIntegrationStack(app, 'SqsIntegrationStack');
